469303a914a6d61e635e4a224914ac94779e8473
paul.adam <paul.adam@epita.fr>
2021-07-15 15:01:08 +0200

    Improved algo

--- src/bdgecc.cc
+++ src/bdgecc.cc
@@ -5,9 +5,9 @@
 #include <algorithm>
 #include <iostream>
 #include <stdbool.h>
+#include <numeric>
 
 
-//FIXME Have a better selection strategy and use igraph_bfs to compute eccentricity and distance at the same time 
 
 void free_complist(igraph_vector_ptr_t *complist, long int index) {
     long int i;
@@ -19,6 +19,143 @@ void free_complist(igraph_vector_ptr_t *complist, long int index) {
     }
 }
 
+void computeCommunities(igraph_t* graph, igraph_vector_t* membership, igraph_integer_t *nb_cluster){
+    //igraph_vector_t  degree;
+    //igraph_real_t quality;
+
+    /* Set default seed to get reproducible results */
+    igraph_rng_seed(igraph_rng_default(), 0);
+
+    /* Perform Leiden algorithm using CPM */
+    //igraph_vector_init(&membership, igraph_vcount(&graph)); TODO Outside of this fonction
+    igraph_community_leiden(graph, NULL, NULL, 0.05, 0.01, 0, membership, nb_cluster, NULL);
+
+    //printf("Membership: ");
+    //igraph_vector_print(membership);
+    //printf("\n");
+
+    /* Start from existing membership to improve it further */
+    //igraph_community_leiden(&graph, NULL, NULL, 0.05, 0.01, 1, &membership, &nb_clusters, &quality);
+
+    //printf("Iterated Leiden, using CPM (resolution parameter 0.05), quality is %.4f.\n", quality);
+    printf("Membership: ");
+    igraph_vector_print(membership);
+    std::cout << " \n Mathieu  : " << igraph_vector_max(membership) << "nb_cluster: " << *nb_cluster;
+    printf("\n");
+
+    /* Initialize degree vector to use for optimizing modularity */
+    //igraph_vector_init(&degree, igraph_vcount(&graph));
+    //igraph_degree(&graph, &degree, igraph_vss_all(), IGRAPH_ALL, 1);
+
+    /* Perform Leiden algorithm using modularity */
+    //igraph_community_leiden(&graph, NULL, &degree, 1.0 / (2 * igraph_ecount(&graph)), 0.01, 0, &membership, &nb_clusters, &quality);
+
+    //printf("Leiden found %" IGRAPH_PRId " clusters using modularity, quality is %.4f.\n", nb_clusters, quality);
+    //printf("Membership: ");
+    //igraph_vector_print(&membership);
+    //printf("\n");
+
+    //igraph_vector_destroy(&degree);
+    //igraph_vector_destroy(&membership);
+    //igraph_destroy(&graph);
+}
+
+
+int selectFromCommunities(igraph_t* V, igraph_vector_t* membership, igraph_integer_t* nb_cluster, bool biggest,std::vector<int> candidate){
+    std::vector<long int> commuSizes(*nb_cluster, 0);
+    std::vector<long int> commuDelegate(*nb_cluster, -1);
+
+    for (long int i = 0; i < igraph_vcount(V); i++){ //DEBUG Maybe error on indexes
+	commuSizes[VECTOR(*membership)[i]] += 1;
+	if (commuDelegate[VECTOR(*membership)[i]] == -1){
+	    if (candidate[i] == 1)
+	        commuDelegate[VECTOR(*membership)[i]] = i;
+	}
+    }
+    if (biggest){
+	for (long int i = 0; i < *nb_cluster; i++){
+	   if (commuDelegate[i] == -1){
+               commuSizes[i] = std::numeric_limits<int>::min();
+	   }
+	}	
+	long int maxCommuIndex = max_element(commuSizes.begin(), commuSizes.end()) - commuSizes.begin();
+	return commuDelegate[maxCommuIndex];
+    }
+    else{
+	for (long int i = 0; i < *nb_cluster; i++){
+	    if (commuDelegate[i] == -1)
+		 commuSizes[i] = std::numeric_limits<int>::max();
+	}
+	long int minCommuIndex = min_element(commuSizes.begin(), commuSizes.end()) - commuSizes.begin();
+	return commuDelegate[minCommuIndex];
+    }    
+}
+
+int selectFromCommunities_cycle(igraph_t* V, igraph_vector_t* membership, igraph_integer_t* nb_cluster, bool biggest,std::vector<int> candidate, int* commuIndex){
+    std::vector<long int> commuSizes(*nb_cluster, 0);
+    std::vector<long int> commuDelegate(*nb_cluster, -1);
+
+    for (long int i = 0; i < igraph_vcount(V); i++){ //DEBUG Maybe error on indexes
+	if (candidate[i] == 1){
+	    commuSizes[VECTOR(*membership)[i]] += 1;
+	    if (commuDelegate[VECTOR(*membership)[i]] == -1)
+	        commuDelegate[VECTOR(*membership)[i]] = i;
+	}
+    }
+    auto remainingClusters = *nb_cluster - std::count(commuDelegate.begin(), commuDelegate.end(), -1); 
+    auto remainingClustersSize = std::accumulate(commuSizes.begin(), commuSizes.end(), 0);
+    if (((float)remainingClustersSize /(remainingClusters * igraph_vcount(V))) > 0.1)
+	    return -1;
+    while (commuDelegate[*commuIndex] == -1){
+        *commuIndex += 1;
+	if (*commuIndex >= *nb_cluster)
+	    *commuIndex = 0;
+    }
+    return commuDelegate[*commuIndex];
+}
+
+int selectFrom(igraph_t V, int v_id, int maxuppernode, int minlowernode, std::vector<int> candidate, bool high, bool biggest, igraph_vector_t* membership, igraph_integer_t* nb_cluster, int* commuIndex){ 
+    int res_id = 0;
+    if(v_id == -1) { // only in the first round, select node with highest degree
+	auto res_neigh = -1;
+        for (int i = 0; i < candidate.size(); i++){
+	    igraph_vector_t i_id_neigh;
+	    igraph_vector_init(&i_id_neigh, 1);
+	    igraph_neighborhood_size(&V, &i_id_neigh,
+                        igraph_vss_1(res_id), 1,
+                        IGRAPH_ALL,
+                        1);
+            auto i_neigh = VECTOR(i_id_neigh)[0];
+	    if (res_neigh == i_neigh)
+	        res_neigh = i_neigh;
+
+            if(i_neigh > res_neigh) {
+                res_id = i;
+		res_neigh = i_neigh;
+            }
+	    igraph_vector_destroy(&i_id_neigh);
+         }
+	 return res_id;
+    }
+    else{
+        res_id = selectFromCommunities(&V, membership, nb_cluster, biggest, candidate);	
+	*commuIndex += 1;
+	if (*commuIndex >= *nb_cluster)
+	    *commuIndex = 0;
+	if (res_id == -1){
+            if(high) // select node with highest upper bound
+                return maxuppernode;
+            else // select node with lowest lower bound
+                return minlowernode;
+	}
+	return res_id;
+    }
+    /*else if(high) // select node with highest upper bound
+        return maxuppernode;
+    else // select node with lowest lower bound
+        return minlowernode;*/
+}
+
 std::vector<int> bdgecc(igraph_t V)
 {
     // Picking the right component
@@ -28,18 +165,18 @@ std::vector<int> bdgecc(igraph_t V)
     long int max_index = 0;
     igraph_integer_t max_vertice = 0;
 
+    //std::cout << max_index;
+    //
+
     long int n = igraph_vector_ptr_size(&complist);
     for (long int i = 0; i < n; i++) {
         igraph_t part = *((igraph_t*)(VECTOR(complist)[i]));
 	auto size = igraph_vcount(&part);
-
 	if (size > max_vertice){
 	    max_vertice = size;
 	    max_index = i;
 	}
     }
-
-    //std::cout << max_index;
 
     free_complist(&complist, max_index);
 
@@ -52,41 +189,25 @@ std::vector<int> bdgecc(igraph_t V)
     std::vector<int> candidate(igraph_vcount(&V), 1);
     int candidates = candidate.size();
     int v_id = -1;
-    bool high = true;
     int maxuppernode = -1;
     int minlowernode = -1;
+    int commuIndex = 0;
 
+    bool high = true;
+    bool biggest = true;
+
+    igraph_vector_t membership; // TODO Free
+    igraph_vector_init(&membership, igraph_vcount(&V));
+    igraph_integer_t nb_cluster; //TODO Free 
+    computeCommunities(&V, &membership, &nb_cluster);
+    //TODO Prune here 
     while(candidates > 0)
     {
 	high = !high;
-        if(v_id == -1) { // only in the first round, select node with highest degree
-            v_id = 0;
-	    auto v_neigh = -1;
-            for (int i = 0; i < candidate.size(); i++){
-	        igraph_vector_t i_id_neigh;
-	        igraph_vector_init(&i_id_neigh, 1);
-	        igraph_neighborhood_size(&V, &i_id_neigh,
-                             igraph_vss_1(v_id), 1,
-                             IGRAPH_ALL,
-                             1);
-		auto i_neigh = VECTOR(i_id_neigh)[0];
-		if (v_neigh == i_neigh)
-	            v_neigh = i_neigh;
-
-                if(i_neigh > v_neigh) {
-                    v_id = i;
-		    v_neigh = i_neigh;
-                }
-            }
-        }
-        else if(high) // select node with highest upper bound
-            v_id = maxuppernode;
-        else // select node with lowest lower bound
-            v_id = minlowernode;
-
+	biggest = !biggest;
+        v_id = selectFrom(V, v_id, maxuppernode, minlowernode, candidate, high, biggest, &membership, &nb_cluster, &commuIndex);
 	maxuppernode = -1;
 	minlowernode = -1;
-       	//igraph_eccentricity(&V, &ecc_vec, igraph_vss_1(v_id), IGRAPH_OUT); // FIXME code ourself eccentricity
 	
 	igraph_vector_t dist;
 
@@ -102,8 +223,9 @@ std::vector<int> bdgecc(igraph_t V)
                NULL, NULL, NULL, NULL, NULL, &dist,
                /*callback=*/ 0, /*extra=*/ 0);
 
-	int v_ecc = (int) igraph_vector_max(&dist);
 	number_of_bfs++;
+
+	int v_ecc = (int) igraph_vector_max(&dist);
         eccs[v_id] = v_ecc;
 
         for (int w_id = 0; w_id < candidate.size(); w_id += 1)
@@ -140,6 +262,8 @@ std::vector<int> bdgecc(igraph_t V)
                              1);
 		if (VECTOR(w_id_neigh)[0] > VECTOR(minlowernode_neigh)[0])
 			minlowernode = w_id;
+
+		igraph_vector_destroy(&minlowernode_neigh);
 	    }
             else if(eccs_lower[w_id] < eccs_lower[minlowernode])
                 minlowernode = w_id;
@@ -154,12 +278,23 @@ std::vector<int> bdgecc(igraph_t V)
                              1);
 		if (VECTOR(w_id_neigh)[0] > VECTOR(maxuppernode_neigh)[0])
 			minlowernode = w_id;
+		igraph_vector_destroy(&maxuppernode_neigh);
 	    }
             else if(eccs_upper[w_id] > eccs_upper[maxuppernode])
                 maxuppernode = w_id;
+	    igraph_vector_destroy(&w_id_neigh);    
         }
+	igraph_vector_destroy(&dist);
 
     }
-    //std::cout << number_of_bfs;
+    igraph_destroy((igraph_t*)VECTOR(*(&complist))[max_index]);
+    igraph_free((igraph_t*)VECTOR(*(&complist))[max_index]);
+    //igraph_destroy(&V);
+    //igraph_free(&V);
+    igraph_vector_ptr_destroy(&complist);
+    igraph_vector_destroy(&membership);
+
+    //igraph_vector_ptr_destroy(&complist);
+    std::cout << '\n' << "Num bfs: " << number_of_bfs << '\n';
     return eccs;
 }
--- src/main.cc
+++ src/main.cc
@@ -1,13 +1,19 @@
 #include "bdgecc.hh"
 #include <iostream>
 #include <vector>
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+
 int main()
 {
     igraph_t g;
-    igraph_vector_t v1, v2;
+    igraph_vector_t v1;
 
     /* simple use */
-    igraph_vector_init(&v1, 16);
+    igraph_vector_init(&v1, 10);
     VECTOR(v1)[0] = 0;
     VECTOR(v1)[1] = 1;
     VECTOR(v1)[2] = 1;
@@ -18,9 +24,32 @@ int main()
     VECTOR(v1)[7] = 2;
     VECTOR(v1)[8] = 2;
 
+    FILE *file;
+
+    file = fopen ("../email-Enron.txt" , "r");
+
+    igraph_t graph;
+    std::cout << "FF14 C est de la merde wow c est mieux";
+
+    igraph_read_graph_edgelist(&graph, file,
+                               0, false);
+
+    std::cout << "Graph created";
+    fclose(file);
     igraph_create(&g, &v1, 0, 0);
-    std::vector<int> eccs = bdgecc(g);
+    std::vector<int> eccs = bdgecc(graph);
     for (auto& i: eccs)
         std::cout << i << ' ';
+    igraph_vector_destroy(&v1);
+    std::cout << '\n' << "Calculated :" << *max_element(eccs.begin(), eccs.end());
+    igraph_real_t res;
+
+    igraph_diameter(&graph, &res,
+                    NULL, NULL,
+                    NULL,
+                    false, true);
+    std::cout << '\n' << "Ground truth: " << res;
+
     igraph_destroy(&g);
-}
+    igraph_destroy(&graph);
+ }
